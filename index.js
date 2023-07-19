ca7924b2cae6a677d9445e05c5affbdce7ee3a9a
Divakaran T <divakarant@Divakarans-MacBook-Air.local>
2023-07-19 20:53:23 +0530

    second commit

--- index.js
+++ index.js
@@ -1,46 +1,116 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const https = require('https');
-const bodyParser = require('body-parser');
+const https = require("https");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.get('/',(req,res)=>{
-    // res.sendFile(__dirname + '/index.html')
-    res.render('index')
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.post('/',function(req,res){
-    const url = 'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple'
-    https.get(url,function(response){
-     response.on('data',function (data){
-        var quizData = JSON.parse(data);
-        res.render('quiz',{quiz: quizData.results})
-     })
-
-    })
-})
-
-app.post('/quiz',function(req,res){
-
-    const result = (req.body)
-
-    console.log(Object.values(result))
-    const ans = Object.values(result)
-    let count = 0
-    ans.map(x => x === 'D' && count++ )
+app.post("/", function (req, res) {
+  //console.log(req.body);
+  const amt = req.body.amount;
+  const diff = req.body.difficulty;
+  const category = req.body.category;
 
 
-    res.render('result',{mark: count})
+  let diffy = 'easy';
 
-})
+  if (diff === 'Hard') {
+    diffy = 'hard'
+  } else if (diff === 'Medium') {
+    diffy = 'medium'
+  }
 
-app.post('/result',function(req,res){
-    res.redirect('/')
-})
+  let type = 9;
+  switch (category) {
+    case "General":
+      type = 9;
+      break;
+    case "Science":
+      type = 17;
+      break;
+    case "Computers":
+      type = 18;
+      break;
+    // case "Mathematics":  API not working!
+    //   type = 19;
+    //   break;
+    case "Geography":
+      type = 22;
+      break;
+    case "History":
+      type = 23;
+      break;
+    case "Sports":
+      type = 21;
+      break;
+    case "Animals":
+      type = 27;
+      break;
+    case "Vehicles":
+      type = 28;
+      break;
+    case "Books":
+      type = 10;
+      break;
+    case "Movies":
+      type = 11;
+      break;
+    case "Celebrities":
+      type = 26;
+      break;
+    // case "Politics":  API not working!
+    //   type = 24;
+    //   break;
 
+    default:
+      type = 9;
+      break;
+  }
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+  const url =
+    "https://opentdb.com/api.php?amount=" + amt + "&category=" + type + "&difficulty=" + diffy + "&type=multiple&encode=url3986";
+  https.get(url, function (response) {
+    response.on("data", function (data) {
+      var quizData = JSON.parse(data);
+      // console.log(quizData.results)
+      var answers = []
+      quizData.results.map(x => {x.incorrect_answers.push(x.correct_answer);
+        shuffle(x.incorrect_answers);
+        answers = [...answers,x.incorrect_answers]});
+        var correct = quizData.results.map(x => x.correct_answer);
+        console.log(correct);
+      res.render("quiz", { quiz: quizData.results , ans:answers, correct:correct, amt:amt });
+    });
+  });
+});
 
-app.listen(3000,function(){
-    console.log('Server started');
-})
+app.post("/quiz", function (req, res) {
+  const result = req.body;
+  const answers = result.btn.split(',');
+  //console.log(result);
+  let count = 0;
+  for (const [key, value] of Object.entries(result)) {
+    if (key != 'btn') {
+      if (answers[key] === value){
+        count++
+      }
+    }
+  }
+  res.render("result", { mark: count ,amount:answers.length});
+});
+
+app.post("/result", function (req, res) {
+  res.redirect("/");
+});
+
+app.listen(3000, function () {
+  console.log("Server started");
+});
